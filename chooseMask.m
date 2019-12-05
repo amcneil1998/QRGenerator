7293264e5e209faf0247e9cc6c7d08bda4581db6
AniXDownLoe <44949550+AniXDownLoe@users.noreply.github.com>
2019-12-05 13:23:11 -0500

    Update chooseMask.m

--- chooseMask.m
+++ chooseMask.m
@@ -3,7 +3,7 @@
 %INPUTS: 21x21 matrix of 1s and 0s that represent the unmasked QR code
 %OUTPUTS: 21x21 matrix of 1s and 0s of the masked QR code, mask used (1-8)
 
-function [masked,maskNb] = chooseMask(matrix);
+function [masked,maskNb] = chooseMask(matrix)
 maskedCode = masking(matrix);
 score = penalties(maskedCode);
 [~,maskNb] = min(score);
